0bb2620bcbe876081236392901a0a64c96b1f3a4
= <43177@PROWAY.TREINA>
2021-09-25 18:03:07 -0300

    Aula2 - Classes

--- aula4.py
+++ aula4.py
@@ -17,8 +17,17 @@ class Produto:
         def ler__nome(self):
             return self.__nome
 
+        #Getter
         def ler__valor(self):
             return self.__valor
+
+        #Setter
+        def configurar_nome(self, nome):
+            self.__nome = nome
+        
+        #Setter
+        def configurar_nome(self, valor):
+            self.__valor = valor
 
 produto = Produto("Refrigerante", 5.80)
 
@@ -28,4 +37,10 @@ produto.__valor = 18.25
 
 print(produto.__nome)
 print(produto.__valor)
+print(produto.ler__nome())
+print(produto.ler__valor())
 # print(produto._categoria)
+
+produto.configurar_nome("Agua Tonica")
+print(produto.ler__nome())
+print(produto)
